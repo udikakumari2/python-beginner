2dd6050b828fa2ff98146d47a9aa1689b09b7a6e
udikakumari2 <udikakumari5@gmail.com>
2023-05-03 14:25:40 -0700

    develop game

--- Car_Game.py
+++ Car_Game.py
@@ -1,10 +1,19 @@
 command=""
+start=False
 while True:
     command=input(">")
     if command.lower()=="start":
-        print("Car started....")
+        if start:
+            print("Car is already started...")
+        else:
+            start=True
+            print("Car started....")
     elif command.lower()=="stop":
-        print("Car stopped....")
+        if not start:
+            Print("Car is already stopped...")
+        else:
+            start=False
+            print("Car stopped....")
     elif command.lower()=="help":
         print("""
         start- to start the car
