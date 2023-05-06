9ae9e4f0b0cddf10d52a8783919b48584adb0f07
udikakumari2 <udikakumari5@gmail.com>
2023-05-06 03:29:13 -0700

    matrix 2d arry

--- For_Loop.py
+++ For_Loop.py
@@ -14,3 +14,17 @@ prices=[10,20,30]
 for price in prices:
     total=total+price
 print(f"Total:{total}")
+#2D list
+matrix=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+#matrix[row][columns]
+print(matrix[0][2])
+#modified
+matrix[0][2]=20
+#print 2d array
+for i in matrix:
+    for item in i:
+        print(item)
